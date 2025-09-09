2328e25ceed109d47e427516927c1c753cad1ff2
Atul <dr6217811@gmail.com>
2025-09-09 18:15:50 +0530

    Add two numbers made from the digits of two lists

--- practice.js
+++ practice.js
@@ -1,26 +1,114 @@
-const obj = {
-    firstName: "Atul",
-    lastName: "Rawat"
+/**
+ * @param {Node} head1
+ * @param {Node} head2
+ * @returns {Node}
+ */
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
 }
+*/
 
-function printName(profession, age) {
-    console.log(this.firstName, this.lastName, profession, age)
-    return 2
-}
-
-
-Function.prototype.myBind = function (obj, ...args2) {
-    let context = obj || globalThis;
-    let that = this;    
-    return function (...args) {
-        let fun = Symbol();
-        context[fun] = that;
-        let result = context[fun](...args2);
-        delete context[fun];
-        return result;
+class Solution {
+    addTwoLists(head1, head2) {
+        // code here
+        function reverse(head) {
+            let current = head, prev = null;
+            while(current) {
+                let next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            return prev;
+        }
+        
+        function trailZero(head) {
+            while(head.next && head.data === 0) {
+                head = head.next;
+            }
+            return head;
+        }
+        
+        head1 = trailZero(head1);
+        head2 = trailZero(head2);
+        
+        head1 = reverse(head1);
+        head2 = reverse(head2);
+        
+        let newHead = null, prev = null, remain = 0;
+        
+        while(head1 && head2) {
+            
+            let num = head1.data + head2.data + remain;
+            
+            if(num > 9) num = num%10, remain = 1;
+            else remain = 0;
+            
+            let newNode = new Node();
+            newNode.data = num;
+            newNode.next = null;
+            
+            if(newHead === null) newHead = newNode, prev = newNode;
+            else prev.next = newNode, prev = newNode;
+            
+            head1 = head1.next, head2 = head2.next;
+            
+        }
+        
+        while(head1) {
+            let num = head1.data + remain;
+            
+            if(num > 9) num = num%10, remain = 1;
+            else remain = 0;
+            
+            let newNode = new Node();
+            newNode.data = num;
+            newNode.next = null;
+            
+            if(newHead === null) newHead = newNode, prev = newNode;
+            else prev.next = newNode, prev = newNode;
+            
+            head1 = head1.next
+        }
+        
+         while(head2) {
+            let num = head2.data + remain;
+            
+            if(num > 9) num = num%10, remain = 1;
+            else remain = 0;
+            
+            let newNode = new Node();
+            newNode.data = num;
+            newNode.next = null;
+            
+            if(newHead === null) newHead = newNode, prev = newNode;
+            else prev.next = newNode, prev = newNode;
+            
+            head2 = head2.next
+        }
+        
+        if(remain) {
+            let newNode = new Node();
+            newNode.data = remain;
+            newNode.next = null;
+            
+            if(newHead === null) newHead = newNode, prev = newNode;
+            else prev.next = newNode, prev = newNode;
+        }
+        
+        
+        newHead = reverse(newHead);
+        
+        return newHead;
     }
 }
 
 
-let newFun = printName.myBind(obj, "Engineer", 32)
-console.log(newFun("Engineer", 32))
+
+
+
