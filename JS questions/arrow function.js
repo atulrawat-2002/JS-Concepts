705c255e11205c51bec7a4a98a4cb2a9ed205205
Atul <dr6217811@gmail.com>
2026-01-28 09:37:02 +0530

    Closeur in js

---
+++ JS questions/arrow function.js
@@ -0,0 +1,64 @@
+// let obj = {
+//     x: 6,
+//     fun: function () {
+//         console.log(this);
+//         function gun(that) {
+//             console.log(that.x);
+//         }
+//         gun(this);
+//     }
+// }
+
+// obj.fun();
+
+// function func() {
+//     console.log(this);
+// }
+
+// normal function
+// func()
+
+
+// arrow function
+// let hi = () => {
+//     console.log(this);
+// }
+
+// hi()
+
+
+// const a = {
+//     age: 23
+// }
+
+// const b = {
+//     gender: "Male"
+// }
+
+// a.func = hi;
+// b.func = hi;
+// b.func();
+
+
+
+// arrow functions are always function expression not declaration
+
+// greet();
+
+
+
+// greet = function (){
+//     console.log("Hi")
+// }
+
+// function atul() {
+//     function add (){
+//         console.log(arguments)
+//     }
+//     add()
+// }
+
+// atul([7, 8])
+
+// x =9
+// console.log(x);
--- JS questions/practice.js
+++ JS questions/practice.js
@@ -1,24 +1,64 @@
-function getThis () {
-  // "use strict";
-  console.log(this);
-  
-  return this;
-};
+// function getThis () {
+//   // "use strict";
+//   console.log(this);
 
-// console.log(getThis());
+//   return this;
+// };
 
-const obj1 = {
-  name: "obj1",
-  arrthis: () => {
-    console.log(this);
-  },
-};
-const obj2 = { name: "obj2" };
+// // console.log(getThis());
 
-obj1.getThis = getThis;
-obj2.getThis = getThis;
+// const obj1 = {
+//   name: "obj1",
+//   arrthis: () => {
+//     console.log(this);
+//   },
+// };
+// const obj2 = { name: "obj2" };
 
-console.log(obj1.getThis());
+// obj1.getThis = getThis;
+// obj2.getThis = getThis;
+
+// console.log(obj1.getThis());
+
+// obj1.arrthis()
+
+// const user = {
+// a: 'atul',
+// tags: ['a', 'b', 'c'],
+// getName () {
+//     this.tags.forEach(function (tag){
+//         console.log(tag, this.a)
+//     }, this)
+// }
+// }
+
+// user.getName()
 
 
-obj1.arrthis()
+
+// function Hi(a, b) {
+//     this.firstName = a
+//     this.lastName = b
+//     console.log(this);
+// }
+
+// new Hi('Atul', 'Rawat')
+
+// class A {
+//     constructor() {
+//         this.firstName = 'Atul'
+//     }
+
+//     getName () {
+//         console.log(this);
+//         function hi() {
+//             console.log(this);
+            
+//         }
+//         hi()
+//     }
+// }
+
+
+// obj = new A()
+// obj.getName();
