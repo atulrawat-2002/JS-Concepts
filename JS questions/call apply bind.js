fc60b7cd38c9df39bb0b6cf4d832fd52d1544ef9
Atul <dr6217811@gmail.com>
2026-02-01 09:43:19 +0530

    Call apply and bind in js

--- JS questions/call apply bind.js
+++ JS questions/call apply bind.js
@@ -1,23 +1,69 @@
-const name1 = {
-    firstname: "Ryan",
-    lastName: "Goslin",
-    
+
+// // function intro(...args) {
+//     console.log(typeof args, args)
+//     // args.forEach((item) => {
+//     //     console.log(`${item}`)
+//     // })
+// }
+
+// const atul = {
+//     name: 'Atul',
+//     age: 23,
+//     sayHello() {
+//         console.log(`Hello I am ${this.name} and nothing much`)
+//     }
+// }
+
+// intro.apply(atul, [atul.name, atul.age])
+
+
+
+// let numbers = [9, 8, 6, 90, 78];
+
+
+
+// // console.log(Math.max.apply(null, ...numbers))
+
+
+// console.log(Math.max(...numbers))
+// const atul = {
+//   name: 'Atul',
+//   age: 23,
+//   sayHello(punctuation, fullStop) {
+//     // console.log(args)
+//     console.log(`Hello I am ${this.name} and nothing much${punctuation} ${fullStop}`);
+//   },
+//   friend: {
+//     name: 'Jack',
+//     age: 33
+//   }
+// };
+
+// atul.sayHello.apply(atul.friend, ['!', '.']);
+// // Hello I am Jack and nothing much
+
+let printFullName = function (city, state) {
+    console.log(`${this.fistName} ${this.lastName} ${city}, ${state}`);
 }
 
-let printFullName = function(age=30, profession="Engineer") {
-        console.log(this.firstname, this.lastName, "is an", profession, "and", age, "years old")
-    }
-
-const name2 = {
-    firstname: "Leonardo",
-    lastName: "Decaprio",
+const obj1 = {
+    fistName: 'Atul',
+    lastName: 'Rawat'
 }
 
-// printFullName.apply(name1, [43, "Actor"])
+const obj2 = {
+    fistName: 'Atul',
+    lastName: 'Rawat'
+}
 
-// printFullName.call(name2, 35, "Actor")
+// printFullName.call(obj1, 'Faridabad', 'Haryana')
 
-let newMethod = printFullName.bind(name1);
+// printFullName.apply(obj2, ['Faridabad', 'Haryana'])
 
-newMethod(30, "Actor")
 
+let myName = printFullName.bind(obj1, );
+// myName('Gujrat', 'Gandhinagar');
+
+obj1.myName = printFullName;
+
+obj1.myName('Faridabad', 'Haryana')
