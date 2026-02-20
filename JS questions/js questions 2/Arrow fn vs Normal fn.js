5a1ce906c07cb469183c9c32d6666d93dd1264bf
Atul <dr6217811@gmail.com>
2026-02-20 16:32:05 +0530

    native http module in nodejs

---
+++ JS questions/js questions 2/Arrow fn vs Normal fn.js
@@ -0,0 +1,26 @@
+
+
+
+
+// this.name = "atul"
+
+
+// function greet(greeting, name) {
+//   console.log(this);
+// }
+
+// const greeting2 = (greeting, name) => {
+//   console.log(this);
+// };
+
+// greet("Good morning", "Atul");
+// greeting2("Good morning", "Atul");
+
+
+function fun() {
+
+}
+
+const gun = a => 8
+
+console.log(gun(8))
