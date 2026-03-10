f1ee67851583076744f0f06df3bbe81a618cd24c
Atul <dr6217811@gmail.com>
2026-03-10 20:52:56 +0530

    practicing generator function in js

--- JS questions/js questions 2/questions/generators.js
+++ JS questions/js questions 2/questions/generators.js
@@ -1,18 +1,38 @@
-// function *generateIds() {
-//     let id = 0;
+// function makeIterator(start = 0, end = Infinity, steps = 1) {
+//     let nextIndex = start;
+//     let iterationCount = 0;
 
-//     while(true) {
-//         yield id++;
+//     const rangeIterator =  {
+//         next() {
+//             let result;
+//             if (nextIndex < end) {
+//                 result = {value: nextIndex, done: false};
+//                 nextIndex += steps;
+//                 iterationCount++;
+//                 return result;
+//             }
+//             return {value: iterationCount, done: true};
+//         }
 //     }
+//     return rangeIterator;
 // }
 
+// let myIteraort = makeIterator(0, 3, 1);
 
+// console.log(myIteraort.next())
+// console.log(myIteraort.next())
+// console.log(myIteraort.next())
+// console.log(myIteraort.next())
 
-function *iterator(array) {
-    for (let i = 0; i < array.length; i++) {
-        yield array[i];
-    }
+function *makeGenerator() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
 }
 
-const gen = iterator([1,2,3,4])
-    
+const myGenerator = makeGenerator();
+
+for (const val of myGenerator) {
+    console.log(val)
+}
