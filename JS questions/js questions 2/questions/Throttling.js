79392736f001f3417c8f90f810abb420b207c271
Atul <dr6217811@gmail.com>
2026-03-27 18:30:56 +0530

    Callback revisited

--- JS questions/js questions 2/questions/Throttling.js
+++ JS questions/js questions 2/questions/Throttling.js
@@ -1,24 +1,16 @@
-function greet () {
-  Object.keys(this).map(item => console.log(item));
+function *generatorFun() {
+  let res = yield new Promise(res => {
+    setTimeout(() => {
+      res(true)
+    }, 1000);
+  });
+
+  return res;
 }
 
 
-let obj = {
-  name: 'Atul',
-  greet: 'Good Morning'
-}
+const gen = generatorFun();
 
 
-
-const myBind = function(context, ...args) {
-    return () => {
-      let fun = Symbol();
-      context[fun] = this;
-      context[fun](...args);
-    }
-}
-
-Function.prototype.myBind = myBind;
-
-let copy = greet.myBind(obj);
-copy()
+console.log(gen.next())
+console.log(gen.next('hello'))
